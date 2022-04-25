ba33a83ac43a808ecb17c7ae977a5a02dd84bfd4
Masum Mollo <freelancermasummolla@gmail.com>
2022-04-25 19:20:01 +0600

    connecte to database

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 6001;
 
@@ -8,6 +9,18 @@ const port = process.env.PORT || 6001;
 app.use(cors());
 app.use(express.json());
 
+
+//dbuser1
+//password: PMsiZ5d1dQEKfzux
+
+const uri = "mongodb+srv://bduser:PMsiZ5d1dQEKfzux@cluster0.9bi2h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log('db connected');
+  // perform actions on the collection object
+  client.close();
+});
 
 
 app.get('/', (req, res) =>{
